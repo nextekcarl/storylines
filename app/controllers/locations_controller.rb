9f3a59832e50aff3d805a635fd43c46e104e1bd0
Carl <nexusrpg@gmail.com>
2009-05-18 11:49:55 -0700

    Can now remove comments on ActiveScaffold configs since the migrations have run on Heroku.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,7 @@
 class LocationsController < ApplicationController
-#  active_scaffold :location do |config|
-#    config.columns = [:name, :description, :events]
-#    config.show.link.inline = false
-#    config.subform.layout = :vertical
-#  end
+  active_scaffold :location do |config|
+    config.columns = [:name, :description, :events]
+    config.show.link.inline = false
+    config.subform.layout = :vertical
+  end
 end
