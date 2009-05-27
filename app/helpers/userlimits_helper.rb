d6e4c329c6345b8f0c8b75fbc3b8d9b7b1d8d099
Carl <nexusrpg@gmail.com>
2009-05-26 19:11:47 -0700

    Trying to change Permissions to Userlimits to hopefully get around the weird problem I started to have deploying on Heroku, forgot to added the renamed files to last commit.

---
+++ app/helpers/userlimits_helper.rb
@@ -0,0 +1,28 @@
+module UserlimitsHelper
+
+  def rights_column(record)
+    record.rights_in_words
+  end
+
+  def rights_form_column(record, input_name)
+    select_tag :record, '<option value="1">view only</option>
+      <option value="2">create and view</option>
+      <option value="3">full edit</option>', {:name => input_name}
+  end
+
+  def user_form_column(record, input_name)
+    @universe = Universe.find(session[:universe_id])
+    @permitted_users = Array.new
+    unless @universe.permissions.nil?
+      @universe.permissions.each do |permission|
+        @permitted_users << permission.user.id
+      end
+    end
+    if @permitted_users.empty?
+      @users = User.find(:all, :conditions => ['id != ?', ["#{current_user.id}"]])
+    else
+      @users = User.find(:all, :conditions => ['id != ? or NOT IN (?)', ["#{current_user.id}", "#{@permitted_users.join(',')}"]])
+    end
+    collection_select :record, :user_id, @users, :id, :username
+  end
+end
