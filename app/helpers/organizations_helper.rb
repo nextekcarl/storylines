ce2a665a37a80997e6e331d89aeaebcebd9e210f
Carl <nexusrpg@gmail.com>
2009-06-11 20:03:13 -0700

    Made the current organization bold in the tree view (when on the Show page).

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -23,14 +23,16 @@ module OrganizationsHelper
     end
   end
 
-  def display_organizations(organization)
-    organization= organization.root
+  def display_organizations(orig_organization)
+    organization= orig_organization.root
 	   ret = "<ul>"
 		   if organization.parent_id == nil
   			ret += "<li>"
-  			ret += link_to organization.name
+  			ret += "<strong>" if organization.id == orig_organization.id
+  			ret += link_to organization.name, :action => 'show', :id => organization.id
+  			ret += "</strong>" if organization.id == orig_organization.id
   			if organization.children.size > 0
-  			  ret += find_all_suborganizations(organization)
+  			  ret += find_all_suborganizations(organization, orig_organization)
 			  end
   			ret += "</li>"
 		   end
@@ -38,18 +40,22 @@ module OrganizationsHelper
 	   ret
     end
 
-   def find_all_suborganizations(organization)
+   def find_all_suborganizations(organization, orig_organization)
     if organization.children.size > 0
       ret = '<ul>'
       organization.children.each { |suborg|
         if suborg.children.size > 0
           ret += '<li>'
+          ret += "<strong>" if suborg.id == orig_organization.id
           ret += link_to h(suborg.name), :action => 'show', :id => suborg
-          ret += find_all_suborganizations(suborg)
+          ret += "</strong>" if suborg.id == orig_organization.id
+          ret += find_all_suborganizations(suborg, orig_organization)
           ret += '</li>'
         else
           ret += '<li>'
+          ret += "<strong>" if suborg.id == orig_organization.id
           ret += link_to h(suborg.name), :action => 'show', :id => suborg
+          ret += "</strong>" if suborg.id == orig_organization.id
           ret += '</li>'
         end
         }
